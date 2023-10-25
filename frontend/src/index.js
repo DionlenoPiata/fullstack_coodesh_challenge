b09d56e3dacb5b21611597c61048455b0a0b0d2b
Dionleno <contato.dionleno@gmail.com>
2023-10-25 10:44:22 -0300

    Adding dark theme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
+root.render(<App />);
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -3,43 +3,48 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import PieChart from "../../components/PieChart";
 import ColumnChart from "../../components/ColumnChart";
 import SearchBar from "../../components/SearchBar";
 import List from "../../components/List";
 
+const darkTheme = createTheme({ palette: { mode: "dark" } });
+
 function Home() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid xs={12}>
-          <Paper sx={{ height: 120, textAlign: "center" }} elevation={3}>
-            <div>Space X</div>
-          </Paper>
+    <ThemeProvider theme={darkTheme}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid xs={12}>
+            <Paper sx={{ height: 120, textAlign: "center" }} elevation={3}>
+              <div>Space X</div>
+            </Paper>
+          </Grid>
+          <Grid xs={12} sm={6}>
+            <Paper sx={{ height: 120, textAlign: "center" }} elevation={3}>
+              <PieChart />
+            </Paper>
+          </Grid>
+          <Grid xs={12} sm={6}>
+            <Paper sx={{ height: 120, textAlign: "center" }} elevation={3}>
+              <ColumnChart />
+            </Paper>
+          </Grid>
+          <Grid xs={12}>
+            <Paper sx={{ height: 60, textAlign: "center" }} elevation={3}>
+              <SearchBar />
+            </Paper>
+          </Grid>
+          <Grid xs={12}>
+            <Paper sx={{ height: 200, textAlign: "center" }} elevation={3}>
+              <List />
+            </Paper>
+          </Grid>
         </Grid>
-        <Grid xs={12} sm={6}>
-          <Paper sx={{ height: 120, textAlign: "center" }} elevation={3}>
-            <PieChart />
-          </Paper>
-        </Grid>
-        <Grid xs={12} sm={6}>
-          <Paper sx={{ height: 120, textAlign: "center" }} elevation={3}>
-            <ColumnChart />
-          </Paper>
-        </Grid>
-        <Grid xs={12}>
-          <Paper sx={{ height: 60, textAlign: "center" }} elevation={3}>
-            <SearchBar />
-          </Paper>
-        </Grid>
-        <Grid xs={12}>
-          <Paper sx={{ height: 200, textAlign: "center" }} elevation={3}>
-            <List />
-          </Paper>
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
 
