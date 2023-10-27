32bf5d9116028a18595486b5b83c67367e86c2c7
Dionleno <contato.dionleno@gmail.com>
2023-10-27 16:45:42 -0300

    Adding chart customization

--- frontend/src/components/PieChart/index.js
+++ frontend/src/components/PieChart/index.js
@@ -5,39 +5,93 @@ import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Chip from "@mui/material/Chip";
 import LabelIcon from "@mui/icons-material/Label";
-import { PieChart as Chart, Pie, Cell } from "recharts";
+import {
+  PieChart as Chart,
+  Pie,
+  Sector,
+  Cell,
+  ResponsiveContainer,
+} from "recharts";
 import LinearProgress from "@mui/material/LinearProgress";
 import axios from "axios";
 
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({
-  cx,
-  cy,
-  midAngle,
-  innerRadius,
-  outerRadius,
-  percent,
-}) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+const renderActiveShape = (props) => {
+  const RADIAN = Math.PI / 180;
+  const {
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent,
+    value,
+  } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? "start" : "end";
 
   return (
-    <text
-      x={x}
-      y={y}
-      fill="white"
-      textAnchor={x > cx ? "start" : "end"}
-      dominantBaseline="central"
-    >
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
+    <g>
+      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+        {payload.name}
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"
+      />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        textAnchor={textAnchor}
+        fill="#333"
+      >{`PV ${value}`}</text>
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={18}
+        textAnchor={textAnchor}
+        fill="#999"
+      >
+        {`${(percent * 100).toFixed(2)}%`}
+      </text>
+    </g>
   );
 };
 
 function PieChart() {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
 
   useEffect(() => {
     fetchData();
@@ -68,7 +122,35 @@ function PieChart() {
             Lançamentos de foguetes
           </Typography>
         </Grid>
-        <Grid xs={4}>
+        <Grid xs={12}>
+          <Box
+            width={"100%"}
+            sx={{ display: "flex", justifyContent: "center" }}
+          >
+            <Chart width={400} height={200}>
+              <Pie
+                activeIndex={activeIndex}
+                activeShape={renderActiveShape}
+                data={data.rockets}
+                cx="50%"
+                cy="50%"
+                innerRadius={50}
+                outerRadius={70}
+                fill="#8884d8"
+                dataKey="count_launches"
+                onMouseEnter={(data, index) => setActiveIndex(index)}
+              >
+                {" "}
+                {data.rockets &&
+                  data.rockets.map((rocket, index) => (
+                    <Cell key={`cell-${index}`} fill={rocket.color} />
+                  ))}
+              </Pie>
+            </Chart>
+          </Box>
+        </Grid>
+
+        <Grid xs={6}>
           <Stack spacing={1}>
             {data.rockets &&
               data.rockets.map((rocket) => (
@@ -87,6 +169,8 @@ function PieChart() {
                 </Typography>
               ))}
           </Stack>
+        </Grid>
+        <Grid xs={6}>
           <Stack sx={{ marginTop: "20px" }} spacing={1}>
             <Typography variant="subtitle2" color="textSecondary">
               <Box
@@ -134,25 +218,6 @@ function PieChart() {
               </Box>
             </Typography>
           </Stack>
-        </Grid>
-        <Grid xs={8}>
-          <Chart width={200} height={200}>
-            <Pie
-              data={data.rockets}
-              cx="50%"
-              cy="50%"
-              labelLine={false}
-              label={renderCustomizedLabel}
-              outerRadius={80}
-              fill="#8884d8"
-              dataKey="count_launches"
-            >
-              {data.rockets &&
-                data.rockets.map((rocket, index) => (
-                  <Cell key={`cell-${index}`} fill={rocket.color} />
-                ))}
-            </Pie>
-          </Chart>
         </Grid>
         {loading && (
           <Grid xs={12}>
