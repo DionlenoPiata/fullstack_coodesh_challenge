cbf3699432b722583a65378d9af5244c895baa8a
Dionleno <contato.dionleno@gmail.com>
2023-10-27 22:06:41 -0300

    Adding endpoint getStatsForBarChart

--- backend/src/controllers/launche-controller.js
+++ backend/src/controllers/launche-controller.js
@@ -68,7 +68,7 @@ exports.getStatsForPieChart = async (req, res, next) => {
         failed_launches: failedLaunches,
       };
 
-      res.send(result);
+      res.status(200).send(result);
     })
     .catch((error) => {
       res.status(404).send({});
@@ -76,5 +76,30 @@ exports.getStatsForPieChart = async (req, res, next) => {
 };
 
 exports.getStatsForBarChart = async (req, res, next) => {
-  res.send({});
+  let rockets = await rocketDao.getAll();
+
+  let result = {
+    rockets: rockets.map((rocker, index) => ({
+      name: rocker.name,
+      color: COLORS[index],
+    })),
+    data_per_year: [
+      {
+        name: "2015",
+        "Foguete A": 1000,
+        "Foguete B": 2000,
+        "Foguete C": 3000,
+        "Foguete D": 4000,
+      },
+      {
+        name: "2016",
+        "Foguete A": 1000,
+        "Foguete B": 2000,
+        "Foguete C": 3000,
+        "Foguete D": 4000,
+      },
+    ],
+  };
+
+  res.send(result);
 };
